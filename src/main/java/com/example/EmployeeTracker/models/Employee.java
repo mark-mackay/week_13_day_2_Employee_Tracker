8ae00950d20d5a247bea0cd8baa7bd3988f6bfc3
Mark Mackay <Mark_Mackay@yahoo.com>
2019-02-26 12:06:01 +0000

    Project and Employee many to many relationship completed and tested

--- src/main/java/com/example/EmployeeTracker/models/Employee.java
+++ src/main/java/com/example/EmployeeTracker/models/Employee.java
@@ -1,7 +1,11 @@
 package com.example.EmployeeTracker.models;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "employees")
@@ -30,7 +34,21 @@ public class Employee {
     @JoinColumn(name = "department_id", nullable = false)
     private Department department;
 
-
+    @JsonIgnore
+    @ManyToMany
+    @JoinTable(
+            name = "employees_projects",
+            joinColumns = { @JoinColumn(
+                    name = "employee_id",
+                    nullable = false,
+                    updatable = false)
+            },
+            inverseJoinColumns = { @JoinColumn(
+                    name = "project_id",
+                    nullable = false,
+                    updatable = false)
+            })
+    private List<Project> projects;
 
 
     public Employee(String name, int age, int employeeNumber, String email, Department department) {
@@ -39,6 +57,7 @@ public class Employee {
         this.employeeNumber = employeeNumber;
         this.email = email;
         this.department = department;
+        this.projects = new ArrayList<>();
     }
 
     public Long getId(){
@@ -89,5 +108,9 @@ public class Employee {
 
     public void setDepartment(Department department) {
         this.department = department;
+    }
+
+    public void addProject(Project project) {
+        this.projects.add(project);
     }
 }
--- src/test/java/com/example/EmployeeTracker/EmployeeTrackerApplicationTests.java
+++ src/test/java/com/example/EmployeeTracker/EmployeeTrackerApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.EmployeeTracker;
 
 import com.example.EmployeeTracker.models.Department;
 import com.example.EmployeeTracker.models.Employee;
+import com.example.EmployeeTracker.models.Project;
 import com.example.EmployeeTracker.repositories.EmployeeRepository;
 import com.example.EmployeeTracker.repositories.DepartmentRepository;
+import com.example.EmployeeTracker.repositories.ProjectRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +21,9 @@ public class EmployeeTrackerApplicationTests {
 	@Autowired
 	DepartmentRepository departmentRepository;
 
+	@Autowired
+	ProjectRepository projectRepository;
+
 	@Test
 	public void contextLoads() {
 	}
@@ -26,10 +31,17 @@ public class EmployeeTrackerApplicationTests {
 	public void createEmployee(){
 		Department marketing = new Department("Marketing");
 		departmentRepository.save(marketing);
+
 		Employee employee1 = new Employee("Mark", 46, 123456789, "mark@yahoo.com", marketing );
 		Employee employee2 = new Employee("Slav", 41, 923456789, "slav@yahoo.com", marketing);
 		employeeRepository.save(employee1);
 		employeeRepository.save(employee2);
+
+		Project icarus = new Project("Icarus", 90);
+		projectRepository.save(icarus);
+
+		icarus.addEmployee(employee1);
+		projectRepository.save(icarus);
 	}
 
 }
